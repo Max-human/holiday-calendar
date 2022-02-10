7a3bba34aedfdea7b621e4f60298809dc730954a
like_johnik <sup3r44ax@yandex.ru>
2022-02-10 20:25:43 +0300

    initial commit

--- setup.py
+++ setup.py
@@ -6,4 +6,9 @@ setup(
     version='1.0',
     packages=find_packages(),
     long_description=open(join(dirname(__file__), 'README.txt')).read(),
-)
+    entry_points={
+        'console_scripts':
+            ['helloworld = helloworld.core:print_message']
+        }
+    )
+
